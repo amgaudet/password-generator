6d7fb02d72e5e987116b488d883b4b55b8688363
Shreekz <alanmguadet@gmail.com>
2021-05-18 19:14:46 -0400

    code formatting, removed extra notes and debug testing lines from js file

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Assignment Code
-// hw notes
-// declare generatePassword() to get something back (the new password)
-// double check variable scope. is password resetting before making a new one?
 var generateBtn = document.querySelector("#generate");
 var lowerLib = "abcdefghijklmnopqrstuvwxyz";
 var upperLib = lowerLib.toUpperCase();
@@ -17,30 +13,26 @@ function writePassword() {
 }
 
 
-function generatePassword(){
-    var newPassword = "";
-    var passLib = "";
-  //password length prompt 8-128 characters
-    var passLength = parseInt(prompt("Choose password length (8-128 characters)"));
+function generatePassword() {
+  var newPassword = "";
+  var passLib = "";
+  var passLength = parseInt(prompt("Choose password length (8-128 characters)"));
 
-  //check if pw length is between 8 and 128
   if (passLength >= 8 && passLength <= 128) {
     console.log(passLength);
   } else {
     alert("Invalid input. Please enter a number between 8 and 128.");
-    writePassword();
+    generatePassword();
   }
 
-  //checks for what character types to add to pw
   var lowerCase = confirm("Would you like to include Lower Case Letters?");
   var upperCase = confirm("Would you like to include Upper Case Letters?");
   var num = confirm("Would you like to include Numbers?");
   var specChar = confirm("Would you like to include Special Characters?");
 
-  //builds library of chars based on client input
   if (!lowerCase && !upperCase && !num && !specChar) {
     alert("Please choose at least one character type for your password");
-    writePassword();
+    generatePassword();
   }
 
   if (lowerCase) {
@@ -56,16 +48,9 @@ function generatePassword(){
     passLib += specialChars;
   }
 
-  console.log(passLib);
-
-  //adds random characters to newPassword based on input length
   for (var i = 0; i < passLength; i++) {
-    //generate random number 0-password library length
     var placeHold = Math.floor(Math.random() * passLib.length);
-
-    //adds character from random spot to password
     newPassword += passLib[placeHold];
-
   }
 
   return newPassword;
